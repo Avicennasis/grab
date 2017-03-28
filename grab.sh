b01b17d3f45c531c847240bcdfcd0424366010bb
Avic <Avicennasis@gmail.com>
2017-03-27 22:42:39 -0400

    Update grab.sh
    Double quotes to prevent globbing and word splitting, using $(..) instead of legacy `..`., read without -r will mangle backslashes

--- grab.sh
+++ grab.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 #Make the directory for Grab logs if it doesn't exist yet
-if [ ! -d ${HOME}/logs/grab ]; then
+if [ ! -d "${HOME}/logs/grab" ]; then
 	echo "Grab logs folder not found! Creating it now."
 	mkdir -p "${HOME}/logs/grab"
 fi
 
 #Make the log file for Grab if it doesn't exist yet
-if [ ! -f ${HOME}/logs/grab/grab.log ]; then
+if [ ! -f "${HOME}/logs/grab/grab.log" ]; then
 	echo "Grab logs file not found! Creating it now."
-	touch ${HOME}/logs/grab/grab.log
+	touch "${HOME}/logs/grab/grab.log"
 fi
 
 #Have we grabbed this site yet? Let's check the logs and go from there.
-a=`grep -rnw ${HOME}/logs/grab/grab.log -e "$1" | wc -l`
+a=$(grep -rnw "${HOME}/logs/grab/grab.log" -e "$1" | wc -l)
 b=0
-if [ ${a} -gt ${b} ]; then 
+if [ "${a}" -gt "${b}" ]; then 
 	echo "I think you already grabbed this site."
 	while true; do
-    read -p "Do you wish to continue this grab? [y/n]" yn
+    read -rp "Do you wish to continue this grab? [y/n]" yn
     case $yn in
         [Yy]* ) wget -e robots=off --recursive --no-parent --reject="index.html*" --user-agent=Mozilla/5.0 --wait=12 --random-wait --directory-prefix=/cygdrive/f/TEMP/ "$1"; break;;
         [Nn]* ) exit;;
@@ -32,7 +32,7 @@ else
 fi
 
 #Echo site to log
-echo $1 >>${HOME}/logs/grab/grab.log
+echo "$1" >>"${HOME}/logs/grab/grab.log"
 
 #Sort log and remove duplicates
-sort -u ${HOME}/logs/grab/grab.log -o ${HOME}/logs/grab/grab.log
+sort -u "${HOME}/logs/grab/grab.log" -o "${HOME}/logs/grab/grab.log"
